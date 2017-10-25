53982a014ed71aa4c924044520af93cff7287d3c
Hector Menendez <hector@gik.mx>
2017-10-25 12:17:21 -0500

    temp

--- test/thrower.js
+++ test/thrower.js
@@ -1,48 +1,48 @@
 import Test from 'ava';
 import Thrower from '../lib/thrower';
 
-Test('throws immediately a TypeError if invalid subject sent', test =>
-    test.throws(() => Thrower(), err =>
+Test('throws immediately a TypeError if invalid subject sent', t =>
+    t.throws(() => Thrower(), err =>
         err.name === 'TypeError' && err.message.indexOf('Invalid subject') === 0,
     ),
 );
 
-Test('returns an unnamed Error instance when sent with only subject', (test) => {
+Test('returns an unnamed Error instance when sent with only subject', (t) => {
     const err = Thrower('Hello', undefined, false);
-    test.is(err.message, 'Hello');
-    test.is(err.name, 'Error');
+    t.is(err.message, 'Hello');
+    t.is(err.name, 'Error');
 });
 
-Test('returns an named Error instance when name parameter sent', (test) => {
+Test('returns an named Error instance when name parameter sent', (t) => {
     const err = Thrower('Hello', 'TestError', false);
-    test.is(err.message, 'Hello');
-    test.is(err.name, 'TestError');
+    t.is(err.message, 'Hello');
+    t.is(err.name, 'TestError');
 });
 
-Test('returns corresponding instance when sent an Error as subject', (test) => {
+Test('returns corresponding instance when sent an Error as subject', (t) => {
     const err = Thrower(new ReferenceError('Aloha'), undefined, false);
-    test.true(err instanceof ReferenceError);
-    test.is(err.message, 'Aloha');
-    test.is(err.name, 'ReferenceError');
+    t.true(err instanceof ReferenceError);
+    t.is(err.message, 'Aloha');
+    t.is(err.name, 'ReferenceError');
 });
 
-Test('returns corresponding instance with custom name, when parameter sent', (test) => {
+Test('returns corresponding instance with custom name, when parameter sent', (t) => {
     const err = Thrower(new RangeError('Prost'), 'SuperTestError', false);
-    test.is(err.message, 'Prost');
-    test.is(err.name, 'SuperTestError');
-    test.true(err instanceof RangeError);
+    t.is(err.message, 'Prost');
+    t.is(err.name, 'SuperTestError');
+    t.true(err instanceof RangeError);
 });
 
-Test('replaces correctly when sent an array as subject', (test) => {
+Test('replaces correctly when sent an array as subject', (t) => {
     const err1 = Thrower(['hello, %s! I am a %s', 'world', 'Test'], undefined, false);
     const err2 = Thrower([], undefined, false);
-    test.is(err1.message, 'hello, world! I am a Test');
-    test.is(err2.message, '');
+    t.is(err1.message, 'hello, world! I am a Test');
+    t.is(err2.message, '');
 });
 
-Test('throws correctly', (test) => {
+Test('throws correctly', (t) => {
     const thrower = () => Thrower('hello', 'world');
-    test.throws(thrower, err =>
+    t.throws(thrower, err =>
         err instanceof Error && err.message === 'hello' && err.name === 'world',
     );
 });
